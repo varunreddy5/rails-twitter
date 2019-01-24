f942d75c5206841b4a1730941d9804adba5e1638
Varun Reddy Regalla <varunreddy1061@gmail.com>
2019-01-23 22:03:16 -0500

    styled the navbar

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render "/profile"
+    end
+  end
+
+
+      #   f.html { redirect_to: root_path, notice: "Post created successfully!" }
+      # else
+      #   f.html { redirect_to: root_path, notice: "Error: Post not saved" }
+      # end
+
+  private
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
+end

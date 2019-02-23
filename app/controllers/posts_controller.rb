7aa70ce9e92f90b24238ced1d10dffb0d4635329
Varun Reddy Regalla <varunreddy1061@gmail.com>
2019-02-23 18:27:55 -0500

    heroku deployment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,14 @@ class PostsController < ApplicationController
       format.js
     end
   end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
+  end
 
 
       #   f.html { redirect_to: root_path, notice: "Post created successfully!" }

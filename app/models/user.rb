4ba86e97cd37d6a184e8fcf72153d3466dccecc1
Varun Reddy Regalla <varunreddy1061@gmail.com>
2019-03-08 14:42:27 -0500

    dependent destroy on User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: "followed"
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
